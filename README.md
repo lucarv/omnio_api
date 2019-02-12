c6423cc82b6b0175d799377769b33abb84c9c73e
lucarv <lucarv@microsoft.com>
2019-02-12 14:41:08 +0100

    change from gateway mode into single device mode

--- README.md
+++ README.md
@@ -1,7 +1,4 @@
 # omnio_api
 
 1. install dependencies (npm i)
-2. edit data/cs.json and enter the connection string
-3. take the files urlencoded.js and json.js and place it under node_modules/body_parser/lib/types, replacing the existing file
-4. take the file index.js and place under node_modules/raw-body
-5. you should now be able to send payloads up to 264 kb
+2. set an environmental variable name CS and assign it to the connection string of the gateway device
--- server.js
+++ server.js
@@ -1,23 +1,25 @@
+'use strict';
+require('dotenv').config()
+
 /* express sdk and MW*/
 const express = require('express');
 var bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-  extended: true
+  extended: true, limit: '300kb'
 }));
 /* azure sdk */
-const iothub = require('azure-iothub');
-const Gateway = require('azure-iot-multiplexing-gateway').Gateway;
-const gateway = new Gateway();
-var Message = require('azure-iot-common').Message;
+var Protocol = require('azure-iot-device-mqtt').Mqtt;
+var Client = require('azure-iot-device').Client;
+var Message = require('azure-iot-device').Message;
 
-var addDevicePromises = [];
-const connectionString = require('./data/cs.json').cs;
-const registry = iothub.Registry.fromConnectionString(connectionString);
-var devices = [];
+//var addDevicePromises = [];
+var connectionString = process.env.CS;
+var client = Client.fromConnectionString(connectionString, Protocol);
+
+//const registry = iothub.Registry.fromConnectionString(connectionString);
 const chalk = require('chalk');
-
 const port = 9999;
 
 
@@ -26,111 +28,41 @@ var server = app.listen(port, function () {
   console.log("app listening at http://%s:%s", server.address().address, server.address().port)
 });
 
-const main = () => {
-  registry.list(function (err, devicesList) {
-    if (err)
-      return err;
-    else {
-      devicesList.forEach((device) => {
-        devices.push(device.deviceId)
-      });
-      startgw();
-    }
-  });
-
-}
-const startgw = async function () {
-  let name = 'startgw';
-  var hubname = connectionString.substring(connectionString.indexOf("=") + 1, connectionString.indexOf(";"));
-
-  console.time('Open tunnel took');
-  try {
-    await gateway.open(connectionString);
-    console.timeEnd('Open tunnel took');
-    console.log('----------------------------------------------------------------------------------------------------------------')
-
-    console.log(chalk.green(`${devices.length} devices provisioned`))
-    if (devices.length > 0) {
-      devices.forEach((deviceId) => {
-        addDevicePromises.push(gateway.addDevice(deviceId));
-      });
-      console.time('Add devices took');
-      await Promise.all(addDevicePromises);
-      console.timeEnd('Add devices took');
-      console.log('----------------------------------------------------------------------------------------------------------------')
-
-      console.log(chalk.green(`cloud gateway started towards ${hubname}`))
-      console.log('----------------------------------------------------------------------------------------------------------------')
-    }
-  } catch (error) {
-    console.log(`${name}: ${error}`);
-  }
-};
-
-/*
-const addDevice = async function (id) {
-  let name = 'addDevice';
-  try {
-    addDevicePromises.push(gateway.addDevice(id));
-    console.time('Add new device took');
-    await Promise.all(addDevicePromises);
-    console.timeEnd('Add new device took');
-    console.log('----------------------------------------------------------------------------------------------------------------')
-  } catch (error) {
-    console.log(`${name}: ${error}`);
-  }
-};
-
-const delDevice = async function (id) {
-  console.time('Delete device took')
-  let name = 'delDevice';
-  try {
-    let detached = gateway.removeDevice(id);
-    let index = addDevicePromises.indexOf(detached);
-    if (index > -1) {
-      addDevicePromises.splice(index, 1);
-    }
-    await Promise.all(addDevicePromises);
-    console.timeEnd('Delete device took');
-    console.log('----------------------------------------------------------------------------------------------------------------')
-  } catch (error) {
-    console.log(`${name}: ${error}`);
-  }
-};
-*/
-const sender = async function (device, message) {
-  console.time('Send message took');
-  let name = 'sender';
-  try {
-    await gateway.sendMessage(device, message);
-    console.log(chalk.green(`** message sent from ${device}`))
-    console.log(chalk.green(`** timestamp: ${new Date()}`))
-
-    console.timeEnd('Send message took');
-    console.log('----------------------------------------------------------------------------------------------------------------')
-  } catch (error) {
-    console.log(chalk.red(`** ${name}: ${error}`));
-  }
-}
-
 app.get('/', function (req, res, next) {
   res.status(200).send('nothing here');
 });
 
 app.post('/messages/:id', function (req, res, next) {
-  console.log(chalk.green(`${new Date()}: message received`))
+  console.log(chalk.green(`${new Date()}: message received from PLC`))
   let id = req.params.id;
-  console.log(req.headers)
+  console.log(req.headers);
+
   let payload = JSON.stringify(req.body);
+  console.log(`size of received payload: ${payload.length}`)
+  var message = new Message(payload);
+  
+  console.time('sending message took')
+
+  client.sendEvent(message, function (err) {
+    if (err) {
+      console.error('Could not send: ' + err.toString());
+      res.status(500).send(err.toString());
+  } else {
+    console.timeEnd('sending message took')
+    res.status(200).send('POST message: ' + req.params.id);
+  }
+  });
+
+  /*
   const index = devices.findIndex(deviceId => deviceId === id);
   if (index == -1)
     res.status(500).send(id + ' not provisioned');
   else {
-      console.log(`size of receiced payload: ${payload.length}`)
-      var message = new Message(payload);
+
       sender(id, message)
       res.status(200).send('POST message: ' + req.params.id);
     } 
+    */
 })
 
-main();
+//main();
