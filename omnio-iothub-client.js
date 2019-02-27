dc0b240ee8bdfb357b0ffeb4f3a09cfbcb77eb34
lucarv <lucarv@microsoft.com>
2019-02-27 21:39:28 +0100

    changed filtering to format {PNU: value}

--- omnio-iothub-client.js
+++ omnio-iothub-client.js
@@ -2,7 +2,8 @@
 require('dotenv').config()
 const debug = require('debug')('omnio-iothub-client')
 const timerPrint = (process.env.timer || false);
-
+const filter = require('./lib/filter')
+//const encoder = require('./lib/encoder')
 /* express SDK and MW*/
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -24,84 +25,13 @@ var client = Client.fromConnectionString(connectionString, Protocol);
 
 /* Filter Constants */
 const port = 9999;
-const params = [ 534, 538, 539, 540, 541, 600, 601, 602, 603, 604, 605, 624 ]
-var bufferedParams = [];
 
 // ----------------
 // Start API server 
 // ----------------
 var server = app.listen(port, function () {
-  debug('api listening at: ' + port)
+  console.log('api listening at: ' + port)
 });
-
-// ----------------------------------------------
-// Store last known values of optional parameters
-// ----------------------------------------------
-const bufferValues = (slaveArray) => {
-  debug(`*** received ${slaveArray.length} new slave readings`)
-  for (let i = 0; i < slaveArray.length; i++) {
-    let sid = slaveArray[i].slaveId;
-    // find if slave already buffered
-    let sidIndex = bufferedParams.findIndex( p => p.slaveId === sid);
-    if (sidIndex == -1)  { // first appearance of this sid 
-      // save last known value of relevant params
-      let temp = {
-        "slaveId": sid,
-        "values": []
-      }
-      for (let j = 0; j < params.length; j++) {
-        let paramIndex = slaveArray[i].datapoints.findIndex( p => p.param === params[j])
-        if ( paramIndex > -1) {
-          let received = slaveArray[i].datapoints[paramIndex]['value'];
-          temp.values.push({ "param": params[j], "value": received, "changed": true })
-        }
-      }
-      bufferedParams.push(temp)
-    } else { // sid already known
-      for (let j = 0; j < params.length; j++) {
-        let paramIndex = slaveArray[i].datapoints.findIndex( p => p.param === params[j])
-        if ( paramIndex > -1) {
-          let stored = bufferedParams[sidIndex].values[j].value;
-          let received = slaveArray[i].datapoints[paramIndex]['value']
-          //debug(`stored: ${stored} - received: ${received}`)
-          if ( stored !== received ) {
-            bufferedParams[sidIndex].values[j].value = received;
-            bufferedParams[sidIndex].values[j].changed = true;
-          } else
-            bufferedParams[sidIndex].values[j].changed = false;
-        }
-      }
-    }
-  }
-};
-
-// --------------------------------------------
-// Remove parameter names and on-changed values
-// --------------------------------------------
-const filterValues = (slaveArray) => {
-  for (let i = 0; i < slaveArray.length; i++) {
-    let sid = slaveArray[i].slaveId;
-    let temp = [];
-    let dpTemp = slaveArray[i].datapoints;
-    for (let j = 0; j < dpTemp.length; j++) {
-      delete dpTemp[j]['name']  
-      temp.push(dpTemp[j])
-    }
-    slaveArray[i].datapoints = temp
-    let bufferIndex = bufferedParams.findIndex( p => p.slaveId === sid)
-    let bufferSid = bufferedParams[bufferIndex];
-    for (let j = 0; j < params.length; j++) {
-      let paramIndex = slaveArray[i].datapoints.findIndex( p => p.param === params[j])
-      if ( paramIndex > -1) {
-        if (!bufferSid.values[j].changed) {
-          debug(bufferSid.values[j])
-          slaveArray[i].datapoints.splice(paramIndex, 1)
-        } 
-      }
-    }
-  }
-  return slaveArray;
-};
 
 app.get('/', function (req, res, next) {
   res.status(200).send('nothing here');
@@ -112,22 +42,24 @@ app.post('/messages/:id', function (req, res, next) {
     console.log('----------------------------------------------------------------------------------------------------------------')
     console.log(`*** ${new Date()}`);
   }
-  let { id } = req.params
+  let {
+    id
+  } = req.params
   let slaveArray = req.body
 
   debug(`*** content-type: ${req.headers["content-type"]}`);
-  if (!timerPrint) 
+  if (!timerPrint)
     console.time('*** prepare message took')
   debug(slaveArray[0]);
-  bufferValues(slaveArray);
-  let filteredArray = filterValues(slaveArray)
-  debug(filteredArray[0]);
-  let payload = JSON.stringify(filteredArray);
-  if (!timerPrint) 
-    console.timeEnd('*** prepare message took')
+  let buffered = filter.bufferValues(slaveArray);
+  let filteredArray = filter.filterValues(slaveArray);
+  let encodedArray = filter.encodePayload(filteredArray);
+  let payload = JSON.stringify(encodedArray);
+  if (!timerPrint)
+    console.timeEnd('*** prepare message took');
   var message = new Message(payload);
-  if (!timerPrint) 
-    console.time('*** sending message took')
+  if (!timerPrint)
+    console.time('*** sending message took');
   client.sendEvent(message, function (err) {
     if (err) {
       console.error(chalk.red('*** Could not send: ' + err.toString()));
